6f14e189debd67df97328969549d589a0ceea6a6
Genius <Genius@LENOVO>
2022-06-01 14:59:07 +0500

    works +_+

--- src/vector.c
+++ src/vector.c
@@ -1,4 +1,5 @@
-#include "vector.h"
+#include "vector.h" 
+#include <assert.h>
 
 typedef struct Node{
     int age;
@@ -7,5 +8,27 @@ typedef struct Node{
 
 
 int main(){
-    Init_Vector(markovChainNode, node)
+    Init_Vector(vector, node)
+    vector vec = vector_new(); 
+    printf("Size: %u Capacity: %u\n", vector_size(&vec), vector_capacity(&vec));
+    node n = {20, 20};
+    vector_push_back(&vec, n);
+    printf("Size: %u Capacity: %u\n", vector_size(&vec), vector_capacity(&vec));
+    node m = {17, 17};
+    vector_push_back(&vec, m);
+    printf("Size: %u Capacity: %u\n", vector_size(&vec), vector_capacity(&vec));
+    vectorResult a = vector_at(&vec, 0);
+    printf("Data: %d\n",((node*)a.node)->pp);
+    assert(!a.error);
+    a = vector_at(&vec, 1);
+    printf("Data: %d\n",((node*)a.node)->pp);
+    assert(!a.error);
+    //a = vector_at(&vec, 2);
+    //assert(!a.error);
+    a = vector_begin(&vec);
+    printf("Data: %d\n",((node*)a.node)->pp);
+    a = vector_end(&vec);
+    printf("Data: %d\n",((node*)a.node - 1)->pp);
+    vector_deinit(&vec);
+    printf("Size: %u Capacity: %u\n", vector_size(&vec), vector_capacity(&vec));
 }
--- src/vector.h
+++ src/vector.h
@@ -1,6 +1,6 @@
 #include <stdlib.h>
-#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 
 #define Init_Vector(typename, type) \
     typedef struct { \
@@ -9,15 +9,15 @@
     } typename; \
     \
     typedef struct { \
-        bool error; \
-        type* node; \
+        int error; \
+        void* node; \
     } typename##Result; \  
  \
     typename typename##_new() { \
         unsigned int capacity = 1; \
         type* ptr_node = malloc(capacity * sizeof(type)); \
         if (!ptr_node) { \
-            fprintf(stderr, "Error in %s", typename##_new); \
+            fprintf(stderr, "Error while malloc'ing in %s", "typename##_new"); \
             exit(1); \
         } \
         typename vector = { ._size = 0, ._capacity = capacity, ._ptr_array = ptr_node }; \
@@ -40,41 +40,43 @@
  \
     typename##Result typename##_begin(typename* vector) { \
         if(vector->_size != 0){ \
-            typename##Result ok = {.error = false, .node = vector->_ptr_array}; \
+            typename##Result ok = {.error = 0, .node = vector->_ptr_array}; \
             return ok; \
         } \
-        typename##Result not_ok = {.error = true}; \
+        typename##Result not_ok = {.error = 1}; \
         return not_ok; \
     } \
  \
     typename##Result typename##_end(typename* vector) { \
         if(vector->_size != 0){ \
-            typename##Result ok = {.error = false, .node = vector->_ptr_array + typename##_size(vector)}; \
+            typename##Result ok = {.error = 0, .node = (vector->_ptr_array + vector->_size)}; \
             return ok; \
         } \
-        typename##Result not_ok = {.error = true}; \
+        typename##Result not_ok = {.error = 1}; \
         return not_ok; \
     } \
  \
     typename##Result typename##_at(typename* vector, unsigned int index) { \
         if(vector->_size != 0 && index >= 0 && index < vector->_size){ \
-            typename##Result ok = {.error = false, .node = vector->_ptr_array[index]}; \
+            typename##Result ok = {.error = 0, .node = vector->_ptr_array + index}; \
             return ok; \
         } \
-        typename##Result not_ok = {.error = true}; \
+        typename##Result not_ok = {.error = 1}; \
         return not_ok; \
-    } \
- \
-    void typename##_push_back(typename* vector, type* node) { \
-        if(vector->_size >= vector->_capacity) \
-            reserve(2 * vector->_capacity); \
-        vector->_ptr_array[vector->_size++] = node; \
     } \
  \
     void typename##_reserve(typename* vector, unsigned int capacity){ \
         type* new_array = realloc(vector->_ptr_array, capacity * sizeof(type)); \
-        
+        if (!new_array) { \
+            fprintf(stderr, "Error while realloc'ing in %s", "typename##_reserve"); \
+            exit(2); \
+        } \
         vector->_ptr_array = new_array; \
         vector->_capacity = capacity; \
     } \
  \
+    void typename##_push_back(typename* vector, type node) { \
+        if(vector->_size >= vector->_capacity) \
+            typename##_reserve(vector, 2 * vector->_capacity); \
+        memcpy((vector->_ptr_array + vector->_size++), &node, sizeof(type)); \
+    } 
